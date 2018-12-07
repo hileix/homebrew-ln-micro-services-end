06295909c28b70eca305f4f3e255faeea0840855
hileix <304192604@qq.com>
2018-12-07 23:01:07 +0800

    first

---
+++ homebrew-ln-micro-services-end.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HomebrewLnMicroServicesEnd < Formula
+  desc "ln-micro-services-end 测试"
+  homepage ""
+  url "https://github.com/hileix/homebrew-ln-micro-services-end/archive/0.1.1.tar.gz"
+  sha256 "9fe180021dacb2881049c5d770d3a176e155d5af292c14c60f8288c3e7e1d4ae"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-ln-micro-services-end`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
